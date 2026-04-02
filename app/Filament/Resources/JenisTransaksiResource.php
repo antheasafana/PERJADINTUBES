c8a5e453bb319d24e006f07369b1e834273afc06
blqsnaaf <blqsnaaf@gmail.com>
2026-04-02 17:16:11 +0700

    Edit kesalahan double

--- app/Filament/Resources/JenisTransaksiResource.php
+++ app/Filament/Resources/JenisTransaksiResource.php
@@ -87,7 +87,6 @@ class JenisTransaksiResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
