19f0ff705e0317a090c4ea23c6e5c76bf474045a
antheasafana <anthea22safana@gmail.com>
2026-05-08 14:44:09 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,65 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+// CONTROLLER
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PengajuanController;
 
-Route::get('/pengajuan', [PengajuanController::class, 'index'])->name('pengajuan.index');
+// =======================
+// LOGIN
+// =======================
 
-Route::get('/pengajuan/create', [PengajuanController::class, 'create'])->name('pengajuan.create');
+Route::get('/login', [
+    AuthController::class,
+    'showLoginForm'
+]);
 
-Route::post('/pengajuan/store', [PengajuanController::class, 'store'])->name('pengajuan.store');
+Route::post('/login', [
+    AuthController::class,
+    'login'
+]);
+
+// =======================
+// LOGOUT
+// =======================
+
+Route::post('/logout', [
+    AuthController::class,
+    'logout'
+]);
+
+// =======================
+// DASHBOARD PEGAWAI
+// =======================
+
+Route::middleware('pegawai')
+->group(function () {
+
+    // DASHBOARD
+    Route::get('/dashboard', function () {
+
+        return view('pengajuan.dashboard');
+
+    });
+
+    // =======================
+    // PENGAJUAN
+    // =======================
+
+    Route::get('/pengajuan', [
+        PengajuanController::class,
+        'index'
+    ])->name('pengajuan.index');
+
+    Route::get('/pengajuan/create', [
+        PengajuanController::class,
+        'create'
+    ])->name('pengajuan.create');
+
+    Route::post('/pengajuan/store', [
+        PengajuanController::class,
+        'store'
+    ])->name('pengajuan.store');
+
+});
